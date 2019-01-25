38d0c5d86731406ceeb8d69649374977bd8fce9a
Daniel Jones <daniel.jones@engineerbetter.com>
2019-01-25 13:34:07 +0000

    configure blobstore write access when creating final release

--- ci/tasks/create-final.sh
+++ ci/tasks/create-final.sh
@@ -8,6 +8,15 @@ VERSION=$(cat version/number)
 
 cd weave-scope-release
 
+cat >> config/private.yml <<EOF
+---
+blobstore:
+  provider: s3
+  options:
+    access_key_id: "$BLOBSTORE_ACCESS_KEY_ID"
+    secret_access_key: "$BLOBSTORE_SECRET_ACCESS_KEY"
+EOF
+
 bosh create-release \
   --final \
   "--tarball=../final-release/weave-scope-final-release-${VERSION}.tgz"
