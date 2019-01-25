0537c9b14733ef59cd21ad629b080a1ed2d82605
Daniel Jones <daniel.jones@engineerbetter.com>
2019-01-25 13:54:09 +0000

    after nine years, I still do not understand S3 permissions

--- ci/tf/buckets.tf
+++ ci/tf/buckets.tf
@@ -18,6 +18,21 @@ resource "aws_s3_bucket" "blobstore" {
   }
 }
 
+resource "aws_s3_bucket_policy" "blob-public-read" {
+  bucket = "${aws_s3_bucket.blobstore.id}"
+
+  policy = <<POLICY
+{
+  "Statement": [{
+    "Action": [ "s3:GetObject" ],
+    "Effect": "Allow",
+    "Resource": "arn:aws:s3:::weave-scope-blobstore/*",
+    "Principal": { "AWS": ["*"] }
+  }]
+}
+POLICY
+}
+
 resource "aws_s3_bucket" "releases" {
   bucket        = "weave-scope-releases"
   acl           = "public-read"
