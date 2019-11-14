8499100e52ddc5935593e0d63d8a39f8bb2ef29c
Andy Paine <andy.paine@engineerbetter.com>
2019-11-14 11:07:20 +0000

    Combine final-release and commit-changes task to avoid copying git repo between tasks
    Signed-off-by: Irbe Krumina <irbekrm@gmail.com>

--- ci/tasks/create-final.sh
+++ ci/tasks/create-final.sh
@@ -5,6 +5,8 @@ set -eu
 export ROOT_PATH=$PWD
 
 VERSION=$(cat version/number)
+git config --global user.email "ci@localhost"
+git config --global user.name "CI Bot"
 
 pushd weave-scope-release
   cat >> config/private.yml <<EOF
@@ -19,6 +21,26 @@ EOF
   bosh create-release \
     --final \
     "--tarball=../final-release/weave-scope-final-release-${VERSION}.tgz"
+
+
+  tag_name="v${VERSION}"
+  tag_annotation="Final release ${VERSION} tagged via concourse"
+  git tag -a "${tag_name}" -m "${tag_annotation}"
+
+  bosh int \
+    manifests/bosh-lite/runtime-config.yml \
+    -o ci/tasks/update-runtime-config-version.yml \
+    -v version=$VERSION \
+    > manifests/bosh-lite/new-runtime-config.yml
+
+  # Can't redirect to file being read
+  mv manifests/bosh-lite/new-runtime-config.yml \
+      manifests/bosh-lite/runtime-config.yml
+
+  git add -A
+  git status
+
+  git commit -m "[ci skip] Adding final release $VERSION via concourse"
 popd
 
-mv weave-scope-release updated-weave-scope-release
+git clone weave-scope-release final-weave-scope-release
